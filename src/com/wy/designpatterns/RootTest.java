f0618c69ddce0909e930fc81f3d796d01fb85d1c
wangyue8 <wantknowtheworld@gmail.com>
2021-01-23 13:12:30 +0800

    设计模式学习项目

---
+++ src/com/wy/designpatterns/RootTest.java
@@ -0,0 +1,15 @@
+package com.wy.designpatterns;
+
+/**
+ * @Author wangyue8
+ * @CreatTime 2021/1/23 13:10
+ **/
+public class RootTest {
+
+  public static void main(String[] args){
+
+
+
+  }
+
+}
