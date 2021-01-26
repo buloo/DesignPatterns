e8be3d118d1ef107c2167b8b3cb7e98bccefa0d3
wangyue8 <wantknowtheworld@gmail.com>
2021-01-27 01:39:38 +0800

    update readme

--- src/com/wy/designpatterns/RootTest.java
+++ src/com/wy/designpatterns/RootTest.java
@@ -25,7 +25,7 @@ public class RootTest {
   public static void main(String[] args){
 
     //策略模式
-    //不同的角色，使用不同的武器，可以随意更换
+    //不同的角色，使用不同的武器，可以随意更换。例如国王默认使用宝剑，但是也可以更换成匕首。
 //    Queen queen = new Queen();
 //    queen.performFight();
 //    queen.setBehavior(new SwordBehavior());//可以安全的改变行为
@@ -34,6 +34,7 @@ public class RootTest {
 
     //观察者模式
     //气象站对应多个显示终端
+    //例如终端1显示气象站的温度、湿度。终端2显示温度、气压。气象站数据发生变化，所有的终端数据也要更新。
 //    WeatherImpl weather = new WeatherImpl();
 //    CurrentConditionsDisplay conditionsDisplay = new CurrentConditionsDisplay(weather);
 //    ThirdpartyDisplay thirdpartyDisplay = new ThirdpartyDisplay(weather);
@@ -41,7 +42,9 @@ public class RootTest {
 //    weather.setMeasurements(36,45,67.4f);
 
     //装饰者模式
-    //咖啡厅
+    //starbuzz咖啡厅
+    //购买咖啡时，可以直接购买基本款，也可以计入各种调料，比如多加一份奶、摩卡等等，
+    //调料也要收费
     Beverage beverage = new EspressoBeverage();
     System.out.println(beverage.getDescription() + " $"+beverage.cost());
 
